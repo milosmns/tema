103a1a141fa71d6898d47057f9c53cf9269c192c
Milos Marinkovic <milosmns@gmail.com>
2020-12-20 13:00:45 +0100

    Build script cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,9 +4,10 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.joda.time.Instant
 import org.joda.time.format.DateTimeFormat
 import java.io.File.separatorChar
-import java.lang.System.getenv
 import java.nio.file.Files
 import java.nio.file.attribute.PosixFilePermission
+
+// region Build script setup
 
 buildscript {
   repositories {
@@ -28,48 +29,51 @@ repositories {
   mavenCentral()
 }
 
-group = "me.angrybyte.kotlin"
-version = "1.0.0"
-val artifact = "tema"
-val sourceName = "native"
-val author = "milosmns"
-val binaryLocation: String
-  get() = "${project.buildDir}${separatorChar}bin${separatorChar}native${separatorChar}temaReleaseExecutable${separatorChar}"
+// endregion
 
-class OS {
-  enum class Type(val longName: String) { MAC("Mac OS"), WINDOWS("Windows"), LINUX("Linux") }
+// region Project configuration
 
-  val current: Type
-    get() = System.getProperty("os.name")?.let { hostOs ->
-      when {
-        hostOs == "Mac OS X" -> MAC
-        hostOs == "Linux" -> LINUX
-        hostOs.startsWith("Windows") -> WINDOWS
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-      }
-    } ?: throw GradleException("Host OS is unknown")
+object Project {
+  val group = Env.get("PROJECT_GROUP", default = "me.angrybyte.kotlin")
+  val artifact = Env.get("PROJECT_ARTIFACT", default = "tema")
+  val version = Env.get("PROJECT_VERSION", default = "1.1.0")
+  val author = Env.get("PROJECT_AUTHOR", default = "milosmns")
+
+  object Location {
+    val binaryDir = "/bin/native/${artifact}ReleaseExecutable/".replace('/', separatorChar)
+  }
+
+  object Source {
+    const val NATIVE = "native"
+  }
 }
+
+group = Project.group
+version = Project.version
+
+// endregion
+
+// region Modules setup
 
 kotlin {
 
-  when (OS().current) {
-    MAC -> macosX64(sourceName)
-    LINUX -> linuxX64(sourceName)
-    WINDOWS -> mingwX64(sourceName)
+  when (OS.current) {
+    MAC -> macosX64(Project.Source.NATIVE)
+    LINUX -> linuxX64(Project.Source.NATIVE)
+    WINDOWS -> mingwX64(Project.Source.NATIVE)
   }.apply {
     binaries {
-      executable(artifact) {
+      executable(Project.artifact) {
         entryPoint = "main"
       }
     }
   }
 
+  @Suppress("UNUSED_VARIABLE") // it's all used
   sourceSets {
 
-    @Suppress("UNUSED_VARIABLE") // reflect
     val nativeMain by getting
 
-    @Suppress("UNUSED_VARIABLE") // reflect
     val nativeTest by getting {
       dependsOn(nativeMain)
       dependencies {
@@ -82,44 +86,44 @@ kotlin {
 
 }
 
+// endregion
+
+// region Tasks setup
+
 tasks {
 
   fun renameBinary() {
-    val extension = when (OS().current) {
+    val extension = when (OS.current) {
       MAC, LINUX -> ".kexe"
       WINDOWS -> ".exe"
     }
 
-    val finalFile = file("$binaryLocation$artifact")
-    if (finalFile.exists()) {
-      finalFile.delete()
-      println("Old binary '${finalFile.absolutePath}' deleted")
+    // locate the binary
+    val binaryLocation = "${project.buildDir}${Project.Location.binaryDir}"
+    val binaryFile = file("$binaryLocation${Project.artifact}$extension").also {
+      if (!it.exists()) error("Missing binary at ${it.absolutePath}")
     }
+    // strip the extension
+    val finalFile = binaryFile.copyTo(
+      target = file("$binaryLocation${Project.artifact}"),
+      overwrite = true
+    ).also {
+      if (!it.exists()) error("Couldn't strip extension at ${it.absolutePath}")
+    }
+    println("Stripped '$extension' from binary at '${finalFile.absolutePath}'")
 
-    val binaryFile = file("$binaryLocation$artifact$extension")
-    if (!binaryFile.exists()) error("Missing binary at ${binaryFile.absolutePath}")
+    if (OS.current == WINDOWS) return
 
-    val copiedFile = binaryFile.copyTo(finalFile)
-    if (!copiedFile.exists()) error("Couldn't strip extension at ${copiedFile.absolutePath}")
-
-    println("Stripped '$extension' from binary at '${copiedFile.absolutePath}'")
-  }
-
-  fun updateBinaryPermissions() {
-    if (OS().current == WINDOWS) return
-    val binaryFile = file("$binaryLocation$artifact")
-    // make executable and grant all permissions
-    binaryFile.setExecutable(true, false)
-    Files.setPosixFilePermissions(binaryFile.toPath(), PosixFilePermission.values().toSet())
+    // make the new binary executable and grant all permissions
+    finalFile.setExecutable(true, false)
+    Files.setPosixFilePermissions(finalFile.toPath(), PosixFilePermission.values().toSet())
+    println("Updated permissions for '${finalFile}'")
   }
 
   val renameBinary by registering {
     description = "Renames the final binary to strip the file extension"
     group = "Publishing"
-    doLast {
-      renameBinary()
-      updateBinaryPermissions()
-    }
+    doLast { renameBinary() }
   }
 
   named("linkTemaReleaseExecutableNative") {
@@ -137,12 +141,15 @@ tasks {
 
 }
 
+// endregion
+
+// region Other plugins
+
 github {
-  val writeToken = getenv("GITHUB_TOKEN") ?: "invalid"
-  if (writeToken == "invalid") println("Set 'GITHUB_TOKEN' environment variable for GitHub releases")
-  val platform = OS().current
-  val platformSuffix = platform.name.first().toLowerCase()
-  val quality = getenv("BUILD_QUALITY") ?: "Debug"
+  val writeToken = Env.get("GITHUB_TOKEN")
+  if (writeToken == Env.INVALID) println("Set 'GITHUB_TOKEN' environment variable to enable GitHub releases")
+  val platformSuffix = OS.current.name.first().toLowerCase()
+  val quality = Env.get("BUILD_QUALITY", default = "Debug")
   val nowTag = DateTimeFormat.forPattern("yyyy_MM_dd_HH_mm_ss")
     .withZoneUTC()
     .let { formatter -> Instant.now().toString(formatter) }
@@ -154,19 +161,50 @@ github {
     "PR" -> "_pr_$nowTag"
     else -> "_dev_$nowTag"
   }
-  val binaryFile = file("$binaryLocation$artifact")
+
+  owner = Project.author
+  repo = Project.artifact
+  token = writeToken
+  tagName = "v${Project.version}_$platformSuffix$qualitySuffix"
+  name = "[${OS.current.longName}] $quality ${Project.version} - $nowName"
+  isPrerelease = quality != "GA"
+
+  val binaryLocation = "${project.buildDir}${Project.Location.binaryDir}"
+  val binaryFile = file("$binaryLocation${Project.artifact}")
+  setAssets(binaryFile.path)
 
   println("Configured for upload: '${binaryFile.absolutePath}'")
-
-  owner = author
-  repo = artifact
-  token = writeToken
-  tagName = "v${project.version}_$platformSuffix$qualitySuffix"
-  name = "[${platform.longName}] $quality ${project.version} / $nowName"
-  isPrerelease = quality != "GA"
-  setAssets(binaryFile.path)
 }
 
 apply {
   plugin("co.riiid.gradle")
 }
+
+// endregion
+
+// region Helpers
+
+object Env {
+  const val INVALID = "<invalid>"
+  fun get(
+    name: String,
+    default: String = INVALID
+  ) = System.getenv(name)
+    .takeIf { !it.isNullOrBlank() }
+    ?: default
+}
+
+object OS {
+  enum class Type(val longName: String) { MAC("Mac OS"), WINDOWS("Windows"), LINUX("Linux") }
+
+  val current: Type = System.getProperty("os.name")?.let { hostOs ->
+    when {
+      hostOs == "Mac OS X" -> MAC
+      hostOs == "Linux" -> LINUX
+      hostOs.startsWith("Windows") -> WINDOWS
+      else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+  } ?: throw GradleException("Host OS is unknown")
+}
+
+// endregion
